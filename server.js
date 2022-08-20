4c4abfbebb6262d9d00a1eb4f5c7700c4091d919
Artsem <artsem.lisouski@sap.com>
2022-08-20 19:40:14 +0300

    Add excel doc search

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require("express")();
+const reader = require('xlsx');
 const PORT = 3000;
 
 app.get("/", (req, res) => {
@@ -7,8 +8,13 @@ app.get("/", (req, res) => {
 
 app.get("/food", (req, res) => {
   const { name } = req.query;
-  
-  res.send(`${name}`);
+
+  const file = reader.readFile('./ABBREV.xlsx');
+  const json = reader.utils.sheet_to_json(
+    file.Sheets[file.SheetNames[0]])
+  const results = json.filter((el) => el.Shrt_Desc.toLowerCase().includes(name.toLowerCase()));
+
+  res.send(`${JSON.stringify(results[0])}`);
 });
 
 app.listen(PORT);
