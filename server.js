3b2144681a0b23bf843ce19d766646754928f85a
Artsem <artsem.lisouski@sap.com>
2022-08-21 11:44:46 +0300

    Add sorting and output

--- server.js
+++ server.js
@@ -12,9 +12,24 @@ app.get("/food", (req, res) => {
   const file = reader.readFile('./ABBREV.xlsx');
   const json = reader.utils.sheet_to_json(
     file.Sheets[file.SheetNames[0]])
-  const results = json.filter((el) => el.Shrt_Desc.toLowerCase().includes(name.toLowerCase()));
+  const results = json.filter((el) => el.Shrt_Desc.toLowerCase().includes(name.toLowerCase())).
+    sort((a, b) => {
+      const aPart = a.Shrt_Desc.toLowerCase().split(name.toLowerCase())[0];
+      const bPart = b.Shrt_Desc.toLowerCase().split(name.toLowerCase())[0];
+      const aInd = aPart ? 1 - (aPart.match(/[,]/g) || []).length : 2;
+      const bInd = bPart ? 1 - (bPart.match(/[,]/g) || []).length : 2;
 
-  res.send(`${JSON.stringify(results[0])}`);
+      return bInd - aInd;
+  });
+  const output = {
+    "name": results[0]["Shrt_Desc"],
+    "kcal, g": results[0]["Energ_Kcal"],
+    "protein, g": results[0]["Protein_(g)"],
+    "lipid, g": results[0]["Lipid_Tot_(g)"],
+    "carbohydrates, g": results[0]["Carbohydrt_(g)"],
+  }
+
+  res.send(`${JSON.stringify(output)}`);
 });
 
 app.listen(PORT);
